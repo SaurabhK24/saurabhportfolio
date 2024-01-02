27780c0bcb50090381b45bfd6979d2f10727f1f5
Saurabh Khanal <Skhanal247@gmail.com>
2024-01-02 02:39:27 -0500

    HeroSection change and tailwind config chagnes.

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,36 +1,80 @@
-import React from 'react'
-import Image from 'next/image'
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 const HeroSection = () => {
-    return (
-        <div>
-            <section>
-                <div className='grid grid-cols-1 lg:gird-cols-12'>
-                    <div className="col-span-7 place-self-center">
-                        <h1 className='text-white text-4xl lg:text-6xl font-extrabold'>
-                            Hello, I'm Saurav!
-                        </h1>
-                        <p className='text-[#ADB7BE] text-lg lg:text-xl py-4'>
-                            This is 24K speaking... come in.
-                        </p>
-                    </div>
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#ee0979] to-[#ff6a00]">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Saurav",
+                1000,
+                "Full Stack SWE",
+                1000,
+                "Equity/Derivative Trader",
+                1000,
+                "Student of the Game",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Hello, I'm Saurav. I am currently working as a junior full stack software engineer. I enjoy working in full stack web development and am interested in Machine Learning and Equity Markets.
+          </p>
+          <div>
+            <Link
+              href="/#contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#ee0979] to-[#ff6a00] hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              href="/"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#ee0979] to-[#ff6a00] hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/headshot.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
+              width={300}
+              height={300}
+            />
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
 
-                    <div className="col-span-5 place-self-center">
-                        <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]">
-                                <Image
-                                src = "/images/headshot.png"
-                                alt='Hero Image'
-                                className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-                                width={300}
-                                height={300}
-                                />
-                        </div>
-                    </div>
-                    
-                </div>
-            </section>
-        </div>
-    )
-}
-
-export default HeroSection 
+export default HeroSection;
