c12ad4cc47422be1ea99171c228de94302113612
Saurabh Khanal <Skhanal247@gmail.com>
2024-01-04 01:08:30 -0500

    Changes in text gradient

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#ee0979] to-[#ff6a00]">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
@@ -44,14 +44,14 @@ const HeroSection = () => {
           <div>
             <Link
               href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#ee0979] to-[#ff6a00] hover:bg-slate-200 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-purple-500 via-pink-500 to-yellow-500 hover:bg-slate-200"
             >
               Hire Me
             </Link>
             <Link
               href={s3Url}
               download="Saurav_CV.pdf"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#ee0979] to-[#ff6a00] hover:bg-slate-800 text-white mt-3"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-500 via-pink-500 to-yellow-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
