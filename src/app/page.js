8d874b144bd08d7bf5120785879fc8b6e96eecfa
Saurabh Khanal <Skhanal247@gmail.com>
2024-01-11 22:57:37 -0500

    Nav Bar Changes
    Fixing Broken About link on NavBar. Full Removal of NavBar aswell.

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,6 @@ import AboutSection from './components/About'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] mx-auto px-12 py-4">
-      <Navbar></Navbar>
       <HeroSection></HeroSection>
       <AboutSection></AboutSection>
       <Footer></Footer>
